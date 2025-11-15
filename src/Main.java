e4edbc455a2dd452c12149b26097ddf6a706381b
Ekaterina <chesnova1509@gmail.com>
2025-11-15 19:34:42 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println();
+
+        int ticketPrice = 3000;
+
+        System.out.println("Стоимость билета" + " " + ticketPrice + " " + "руб.");
+        System.out.println("У нас работает бонусная программа, по которой за каждые 20 рублей, потраченные на билет, начисляется 1 миля");
+
+        int valueOfMile = 20;
+
+        int bonusMiles = ticketPrice / valueOfMile;
+
+        System.out.println("Ваш бонус" + " " + bonusMiles + " " + "миль");
+
+
+
     }
 }
